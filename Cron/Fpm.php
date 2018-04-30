0e0dd7a06b56d97f536a2fafd1fe2b1342582361
Chris Onderwater <chrisonderwater@gmail.com>
2018-04-30 15:58:17 +0200

    Export generated rules to database table in cron job

--- Cron/Fpm.php
+++ Cron/Fpm.php
@@ -12,9 +12,16 @@ class Fpm {
      * @var |Onderwater|TransactionMining|Model|Transactions
      */
     protected $_transactions;
+    protected $_rules;
+    protected $_productRepository;
+    protected $_objectManager;
 
-    public function __construct(\Onderwater\TransactionMining\Model\Transactions $transactions) {
+    public function __construct(\Onderwater\TransactionMining\Model\Transactions $transactions,
+    \Onderwater\TransactionMining\Model\Rules $rules, \Magento\Catalog\Api\ProductRepositoryInterface $productRepository) {
         $this->_transactions = $transactions;
+        $this->_rules = $rules;
+        $this->_productRepository = $productRepository;
+        //$this->_objectManager = $objectManager;
     }
 
 /**
@@ -29,6 +36,20 @@ class Fpm {
         $apriori = new Apriori(0, 0.5);
         $apriori->train($transactions, []);
         $rules = $apriori->getRules();
+
+        //Get Object Manager Instance
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        foreach($rules as $rule){
+            //$product = $this->productRepository->get($sku);
+            $rules = $objectManager->create('Onderwater\TransactionMining\Model\Rules');
+            $rules->setAntecedent(implode(",", $rule["antecedent"]));
+            $rules->setConsequent(implode(",", $rule["consequent"]));
+            $rules->setSupport($rule["support"]);
+            $rules->setConfidence($rule["confidence"]);
+            $rules->save();
+        }
+
         return $rules;
 
         // Sample for iterating through items.
